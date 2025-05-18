c75e998afd8f25f9f3cef6ec08c3e267387b2cae
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-05-19 01:24:53 +0530

    design added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [todolist,setTodolist]=useState([]);
+
+  let saveToDoList=(event)=>{
+    event.preventDefault();
+
+    let toname=event.target.toname.value;
+    if(!todolist.includes(toname))
+    {
+      let finalDolist=[...todolist,toname];
+      setTodolist(finalDolist);
+    }
+    else
+    {
+      alert("todolist already exist");
+    }
+  }
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <form>
-        <input type="text"/><button>save</button>
+      <form onSubmit={saveToDoList}>
+        <input type="text" name="toname"/><button>save</button>
       </form>
+      <div className='outerDiv'>
+        <ul>
+          <li>HTML <span>&times;</span></li>
+        </ul>
+      </div>
     </div>
   );
 }
